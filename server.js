57d0d39814bf9be933934fed5fe5c868ba5ecd05
Smaran Harihar <smaran.harihar@gmail.com>
2021-08-02 13:55:14 -0700

    integrate mongoose

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import pkg from "dotenv";
+
 const app = express();
+const { config } = pkg;
+
+config();
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to database'));
 
 app.listen(4200, () => console.log("Server is running"));
